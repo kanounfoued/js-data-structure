b055f50ce8751408772de0d885799a739be08560
kanounfoued <fouad.kanoun@legal-doctrine.com>
2021-06-08 16:46:04 +0100

    :hammer: run some example about building heaps

---
+++ index.ts
@@ -0,0 +1,6 @@
+import Build_Heap_Structure from "./build_heap_structure";
+
+const arr_heap: number[] = [4, 6, 2, 0, 7, 90, 34, 1];
+
+const t: Build_Heap_Structure<number> = new Build_Heap_Structure<number>();
+t.buildMaxHeap(arr_heap);
