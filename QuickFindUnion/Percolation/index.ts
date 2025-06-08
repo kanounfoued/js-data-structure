85acfe162f0de5bd3a3c33c117e57588bf6019c9
kanounfoued <foued.kanoun@openinnovation.ai>
2025-06-09 00:45:32 +0400

    feat: implement a percolation system class to check if a system percolates

--- QuickFindUnion/Percolation/index.ts
+++ QuickFindUnion/Percolation/index.ts
@@ -23,14 +23,12 @@ class PercolateSystem {
    */
   constructor(n: number) {
     this.weightedQuickUnion = new WeightedQuickUnionWithPathCompression(n * n);
-    this.gridCell = new GridCellNeighbor(n * n);
+    this.gridCell = new GridCellNeighbor(n);
     this.numOfOpenSites = 0;
 
-    for (let i = 0; i < n; i++) {
-      for (let j = 0; j < n; j++) {
-        this.grid[i][j] = 0;
-      }
-    }
+    this.grid = Array(n)
+      .fill(0)
+      .map(() => Array(n).fill(0));
   }
 
   // opens the site (row, col) if it is not open already
@@ -45,7 +43,7 @@ class PercolateSystem {
     // get the neighbors of the current position.
     const neighbors = this.gridCell
       .getNeighbors(row, col)
-      .filter((cell) => !this.isOpen(cell.row, cell.col));
+      .filter((cell) => this.isOpen(cell.row, cell.col));
 
     if (neighbors.length === 0) {
       return;
@@ -66,7 +64,24 @@ class PercolateSystem {
 
   // is the site (row, col) full?
   // it needs to verify if there is a path from the current position to the top row.
-  public isFull(row: number, col: number) {}
+  public isFull(row: number, col: number) {
+    for (let index = 0; index < this.grid[0].length; index++) {
+      if (!this.isOpen(0, index)) {
+        continue;
+      }
+
+      const areConnected = this.weightedQuickUnion.connected(
+        row * this.grid.length + col,
+        index
+      );
+
+      if (areConnected) {
+        return true;
+      }
+    }
+
+    return false;
+  }
 
   // returns the number of open sites
   public numberOfOpenSites() {
@@ -76,23 +91,63 @@ class PercolateSystem {
   // does the system percolate?
   // the system percolates if there is a full site starting from the top to the bottom.
   public percolates(): boolean {
+    const lastRow = this.grid.length - 1;
+
+    for (let index = 0; index < this.grid[lastRow].length; index++) {
+      if (!this.isOpen(lastRow, index)) {
+        continue;
+      }
+
+      if (this.isFull(lastRow, index)) {
+        return true;
+      }
+    }
+
     return false;
   }
 }
 
+function availableCells(n: number) {
+  const cellRegister: Array<Array<number>> = new Array(n)
+    .fill(0)
+    .map(() => new Array(n).fill(0));
+
+  for (let row = 0; row < n; row++) {
+    for (let col = 0; col < n; col++) {
+      cellRegister[row][col] = row * n + col;
+    }
+  }
+
+  return cellRegister;
+}
+
 function main() {
-  const n = 20;
+  const n = 4;
   const percolateSystem = new PercolateSystem(n);
-  const numberOfShoot = (n * n) / 2;
+  const numberOfShoot = n * n;
+
+  let cellRegister = availableCells(n);
 
   for (let i = 0; i < numberOfShoot; i++) {
-    const row = Math.floor(Math.random() * n);
-    const col = Math.floor(Math.random() * n);
+    // let row = Math.floor(Math.random() * (cellRegister.length - 1));
+    let row = undefined;
+
+    do {
+      row = Math.floor(Math.random() * cellRegister.length);
+    } while (cellRegister[row].length === 0);
+
+    const random = Math.floor(Math.random() * (cellRegister[row].length - 1));
+    const randomNumber = cellRegister[row][random];
+
+    const col = Math.floor(randomNumber - row * n);
+
+    cellRegister[row].splice(random, 1);
 
     percolateSystem.open(row, col);
     const isPercolating = percolateSystem.percolates();
 
     if (isPercolating) {
+      console.log("isPercolating", isPercolating);
       break;
     }
   }
